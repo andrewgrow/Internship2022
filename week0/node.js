a3fd01cb40e933ae5a96699744ce7d41833ecbac
Andrew Gahov <andrew.gahov@gmail.com>
2022-11-08 14:49:25 +0200

    store file by ENV variables

--- week0/node.js
+++ week0/node.js
@@ -4,29 +4,30 @@
  */
 const fs = require('fs');
 
-async function getUsersByApi() {
-    return Promise.resolve('https://jsonplaceholder.typicode.com/users')
+async function getDataByApi(url) {
+    return Promise.resolve(url)
         .then(async (url) => {
             return await fetch(url);
         })
         .then(async (response) => {
             if (response.ok !== null && response.ok !== undefined) {
-                return await response.json();
+                const data = await response.json();
+                return JSON.stringify(data);
             } else {
                 throw new Error('Response from remote server is bad.');
             }
         });
 }
 
-function writeDataToFile(data) {
-    fs.writeFile('users.json', data, function (err) {
+function writeDataToFile(path, data) {
+    fs.writeFile(path, data, function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
 }
 
-getUsersByApi()
-    .then((response) => writeDataToFile(JSON.stringify(response)))
+getDataByApi('https://jsonplaceholder.typicode.com/users')
+    .then((data) => writeDataToFile('users.json', data))
     .catch((err) => console.error(err));
 
 
@@ -37,3 +38,12 @@ getUsersByApi()
  * If param is PRODUCTION  get data from https://jsonplaceholder.typicode.com/todos and write it to file todos.json
  * If param is DEV get data from https://jsonplaceholder.typicode.com/albums and write if to file albums.json
  */
+
+require('dotenv').config();
+const VARIABLES = {};
+VARIABLES.url = process.env.ENV === 'PRODUCTION' ? 'https://jsonplaceholder.typicode.com/todos' : 'https://jsonplaceholder.typicode.com/albums';
+VARIABLES.file = process.env.ENV === 'PRODUCTION' ? 'todos.json' : 'albums.json';
+
+getDataByApi(VARIABLES.url)
+    .then((data) => writeDataToFile(VARIABLES.file, data))
+    .catch((err) => console.error(err));
