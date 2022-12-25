154fa8653b7c8274d4b90e9604ed7c48c7fc9cc5
Andrew Gahov <andrew.gahov@gmail.com>
2022-12-25 21:22:39 +0200

    add migration 'set task status' (2)

--- src/db/migrations/20221225184519-set_task_status.js
+++ src/db/migrations/20221225184519-set_task_status.js
@@ -1,7 +1,7 @@
 module.exports = {
   async up(db, client) {
-      await db.collection('tasks').updateMany({estimatedTime: { $lte: 10 }}, {$set: { status: 'in progress' }});
-      await db.collection('tasks').updateMany({estimatedTime: { $gt: 10 }}, {$set: { status: 'done' }});
+      await db.collection('tasks').updateMany({estimatedTime: { $lte: 10 }}, {$set: { status: 'done' }});
+      await db.collection('tasks').updateMany({estimatedTime: { $gt: 10 }}, {$set: { status: 'in progress' }});
   },
 
   async down(db, client) {
